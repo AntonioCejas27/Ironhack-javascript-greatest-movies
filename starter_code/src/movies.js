0fd6964c064fc6dd391ef3f3ba26eb2079fb2cf0
Antonio Cejas <22072@columbushs.com>
2019-11-22 08:45:33 -0500

    Still need to finish

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -2,9 +2,46 @@
 
 // Iteration 1: Ordering by year - Order by year, ascending (in growing order)
 
-// Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+movies.orderbyYear([anArray]){
+    r
 
-// Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+    movies.year.sort()
+    console.log()
+
+    // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct
+
+    function howManyMovies(anArray) {
+        if (genre == "drama" && director == "steven speilberg") {
+            return
+        }
+    }
+
+
+    function howManyMovies(anArray) {
+        anArray.filter((eachMovie) => {
+        }
+    }
+
+    // Iteration 3: Alphabetic Order - Order by title and print the first 20 titles
+
+    function orderAlphabetically(array) {
+
+        let copy = [...array];
+
+        copy.sort((a, b) => {
+            if (a.title < b.title) {
+                return -1
+            }
+            else if (b.title, a.title) {
+                return 1;
+            }
+
+            else{
+            return 0;
+
+        }
+
+    }
 
 // Iteration 4: All rates average - Get the average of all rates with 2 decimals
 
@@ -12,27 +49,4 @@
 
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
-// BONUS Iteration: Best yearly rate average - Best yearly rate average
-
-movies.sort ((a, b) =>{
-    return b-a;
-  });
-
-  console.log(movies)
-}
-
-function howManyMovies(anArray){
-    if(genre == "drama" && director == "steven speilberg") {
-        return
-    }
-}
-
-function howManyMovies(anArray) {
-    anArray.filter( (eachMovie)=>{
-        return 
-    })
-}
-
-
-
-
+// BONUS Iteration: Best yearly rate average - Best yearly rate average 
